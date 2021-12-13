e482b0521e72a9fce06d17759720bce755df8653
Sarang S. Bhagwat <sarangb2@illinois.edu>
2021-12-13 12:18:35 -0600

    Fix path cleanup functionality

--- bottlenecks.py
+++ bottlenecks.py
@@ -14,20 +14,8 @@ import cProfile
 import pstats
 import pandas
 
-def get_bottlenecks_full_list(funct):
-    profile=cProfile.Profile()
-    profile.runcall(funct)
-    # stats = cProfile.runctx("funct()", locals(), globals(), filename)
-    # stats = pstats.Stats(profile)
-    # stats.sort_stats('cumulative')
-    # stats.reverse_order()
-    # stats.print_stats()
-    stats = profile.getstats()
-    statslist = [(i[0], i[1], i[3], i[4]) for i in stats]
-    statslist.sort(key=lambda i: i[2], reverse=True)
-    return statslist
-
-def get_bottlenecks(funct, filename=None, length=80, max_timefrac_for_singlecallers=0.95, remove_from_path=None):
+# Main entry point
+def get_bottlenecks(funct, filename=None, length=80, max_timefrac_for_singlecallers=0.95, remove_all_before_this_from_path=None):
     statslist = get_bottlenecks_full_list(funct=funct)
     total_time = statslist[0][2]
     indices_to_remove = (statslist.index(i) for i in statslist 
@@ -35,14 +23,23 @@ def get_bottlenecks(funct, filename=None, length=80, max_timefrac_for_singlecall
     for index in indices_to_remove:
         statslist.remove(statslist[index])
     statslist=statslist[0:length]
-    statsdict = {get_keys(i[0])[0]: {'Path': get_keys(i[0], remove_from_path=remove_from_path)[1],
+    statsdict = {get_keys(i[0])[0]: {'Path': get_keys(i[0], remove_all_before_this_from_path=remove_all_before_this_from_path)[1],
                                      'N_calls': i[1], 'Mean_time_per_call [s]': i[2]/i[1],
                                      'Cumulative_time [s]': i[2]} for i in statslist}
     df = pandas.DataFrame(statsdict).transpose()
     df.to_csv(filename+'.csv')
     return df
+#
 
-def get_keys(codeobject, function_name_start_index=13, path_start_identifier='"', remove_from_path=None):
+def get_bottlenecks_full_list(funct):
+    profile=cProfile.Profile()
+    profile.runcall(funct)
+    stats = profile.getstats()
+    statslist = [(i[0], i[1], i[3], i[4]) for i in stats]
+    statslist.sort(key=lambda i: i[2], reverse=True)
+    return statslist
+
+def get_keys(codeobject, function_name_start_index=13, path_start_identifier='"', remove_all_before_this_from_path=[]):
     orig_str = str(codeobject)
     if 'code object' in orig_str:
         trunc_str = orig_str[function_name_start_index:-1]
@@ -54,16 +51,17 @@ def get_keys(codeobject, function_name_start_index=13, path_start_identifier='"'
                 break
         path_start_index = trunc_str.index(path_start_identifier)
         path_str = trunc_str[path_start_index:]
-        if not remove_from_path:
+        if not remove_all_before_this_from_path:
             return function_name_str, path_str
         else:
-            if type(remove_from_path)==str:
-                remove_from_path=[remove_from_path]
-            for substr in remove_from_path:
-                path_str = get_str_with_substr_removed(path_str, substr)
+            if type(remove_all_before_this_from_path)==str:
+                remove_all_before_this_from_path=[remove_all_before_this_from_path]
+            for substr in remove_all_before_this_from_path:
+                # path_str = get_str_with_substr_removed(path_str, substr)
+                path_str = get_str_with_everything_before_substr_removed(path_str, substr)
+            if not path_str[0]=='"':
+                path_str = '"...' + path_str
             return function_name_str, path_str
-    # elif 'built-in' in orig_str:
-    #     return 'built-in method', orig_str.rsplit('built-in method')[1][1:]
     else:
         return orig_str, None
     
@@ -71,5 +69,12 @@ def get_str_with_substr_removed(orig_str, substr):
     if substr in orig_str:
         splitstr = orig_str.rsplit(substr)
         return splitstr[0] + splitstr[1]
+    else:
+        return orig_str
+
+def get_str_with_everything_before_substr_removed(orig_str, substr):
+    if substr in orig_str:
+        splitstr = orig_str.rsplit(substr)
+        return splitstr[1]
     else:
         return orig_str
